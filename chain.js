27a010e983902182a883a087b61ba0e0825ed02c
Niharcoo <86790782+Niharcoo@users.noreply.github.com>
2021-07-17 20:25:21 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,31 @@
+class Chain{
+     constructor(object1,object2){
+          var option={
+            bodyA:object1,
+            bodyB:object2,
+            length:10,
+            stiffness:0.5
+
+
+
+          }
+      this.chain=Constraint.create(option);
+      World.add(world,this.chain);
+
+
+     }
+
+     display(){
+      var pointA=this.chain.bodyA.position;
+      var pointB=this.chain.bodyB.position;
+      strokeWeight(3)
+      line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+     }
+
+
+
+
+     
+
+}
